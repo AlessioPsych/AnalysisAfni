ad698fb537ebcc4daf4537fbe5173a952d663852
AlessioPsych <alessio.fracasso@gmail.com>
2025-10-11 14:01:08 +0100

    updated surface metrics computation

--- surfaces/metricSurfaceMap.R
+++ surfaces/metricSurfaceMap.R
@@ -1,32 +1,35 @@
 args <- commandArgs(T)
 print( args )
 
+inputMetric <- args[1]
+inputSurfaceFolder <- args[2]
+
 #setwd('/media/alessiofracasso/storage2/dataLaminar_leftVSrightNEW/V2677leftright_Ric/resultsCBS')
 #args <- c( '-node_normals' )
 
-surfaceFilesTopo <- dir( 'surfaces_folder/', pattern='*.topo'  )
+surfaceFilesTopo <- dir( inputSurfaceFolder, pattern='*.topo'  )
 surfaceFilesTopo <- surfaceFilesTopo[1:length(surfaceFilesTopo)-1]
-surfaceFilesCoords <- dir( 'surfaces_folder/', pattern='*.coord'  )
+surfaceFilesCoords <- dir( inputSurfaceFolder, pattern='*.coord'  )
 surfaceFilesCoords <- surfaceFilesCoords[1:length(surfaceFilesCoords)-1]
-surfaceFileSpec <- dir( 'surfaces_folder/', pattern='spec*'  )
+surfaceFileSpec <- dir( inputSurfaceFolder, pattern='spec*'  )
 
-dirSplit <- strsplit(args[1],'[-]')
-newDir <- sprintf('surface_%s', dirSplit[[1]][2] )
+dirSplit <- strsplit( inputMetric,'[-]' )
+newDir <- sprintf('%s_%s', inputSurfaceFolder, dirSplit[[1]][2] )
 
 ## checkdir, create if it does not exists, if it exists then halts execution
 mainDir <- getwd()
 flagDir <- dir.create( file.path(mainDir, newDir) )
 if (flagDir==FALSE) { # directory already exists!
-  msg <- sprintf( 'Remove the directory %s_folder to proceed', args[1] )
+  msg <- sprintf( 'Remove the directory %s to proceed', newDir )
   warning( msg )
   stopifnot(flagDir)  
 }
 
 for ( k in 1:length(surfaceFilesCoords) ) {
   outSplitName <- strsplit(surfaceFilesCoords[k],'[.]')
-  outputFile <- sprintf( '%s/%s.normals', newDir, outSplitName[[1]][1] )
-  instr <- sprintf('SurfaceMetrics %s -i_1D surfaces_folder/%s surfaces_folder/%s -prefix %s',
-                   args[1], surfaceFilesCoords[k], surfaceFilesTopo[k], outputFile )  
+  outputFile <- sprintf( '%s/%s.metric', newDir, outSplitName[[1]][1] )
+  instr <- sprintf('SurfaceMetrics %s -i_1D %s/%s %s/%s -prefix %s',
+                   inputMetric, inputSurfaceFolder, surfaceFilesCoords[k], inputSurfaceFolder, surfaceFilesTopo[k], outputFile )  
   if (k<10) { system( sprintf( 'echo surface n. 0%s, files: %s %s', k, surfaceFilesCoords[k], surfaceFilesTopo[k] ) ) }
   if (k>=10) { system( sprintf( 'echo surface n. %s, files: %s %s', k, surfaceFilesCoords[k], surfaceFilesTopo[k] ) ) }
   
--- surfaces/metricSurfaceMap.sh
+++ surfaces/metricSurfaceMap.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 METRIC=$1
+SURFACEFOLDER=$2
 
 if [ -z "$1" ]
 then
 echo 'computes surface metric on a surface set (directory)'
 echo 'Inputs:'
-echo 'METRIC=$1, afni SurfaceMetrics flag (with dash)'
+echo 'METRIC=$1, afni SurfaceMetrics flag (with dash), see SurfaceMetrics help for specifics'
+echo 'SURFACEFOLDER=$2, surface folder over which to compute the selected metric'
 exit 1
 fi
 
-Rscript $AFNI_TOOLBOXDIRSURFACES/metricSurfaceMap.R $METRIC
+Rscript $AFNI_TOOLBOXDIRSURFACES/metricSurfaceMap.R $METRIC $SURFACEFOLDER
